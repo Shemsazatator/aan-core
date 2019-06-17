f70420ccd010ffd79e43247ae025bb70547be499
aecy <devsnake@hotmail.com>
2019-06-17 14:22:38 +0200

    Doesnt need signFile

--- ui/includes.cpp
+++ ui/includes.cpp
@@ -1,7 +1,4 @@
-/*----------------------------------------
-    Author:		ArmApps
-    Website:	https://armapps.net
-------------------------------------------*/
+
 #include "\a3\3den\UI\dikCodes.inc"
 #include "\a3\3den\UI\macros.inc"
 #include "\a3\3den\UI\macroExecs.inc"
